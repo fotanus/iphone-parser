433ceecf8d501d5d3b4813d28cdfc76d836835c2
fotanus@gmail.com <Felipe Tanus>
2014-12-31 08:28:51 -0200

    Add optional semicolon test case

--- spec/lib/iphone_parser_spec.rb
+++ spec/lib/iphone_parser_spec.rb
@@ -53,6 +53,23 @@ describe IphoneParser do
     expect(entries[1][:text]).to eq("text 2")
   end
 
+  it "semicolon is optional" do
+    resource_file = <<-eof
+    // comment 1
+    "label 1" = "text 1"
+    /* comment 2 */
+    "label 2" = "text 2"
+    eof
+    entries = IphoneParser.parse(resource_file)
+    expect(entries.count).to eq(2)
+    expect(entries[0][:comments]).to match(/\/\/ comment 1\n/)
+    expect(entries[0][:label]).to eq("label 1")
+    expect(entries[0][:text]).to eq("text 1")
+    expect(entries[1][:comments]).to match(/\/\* comment 2 \*\//)
+    expect(entries[1][:label]).to eq("label 2")
+    expect(entries[1][:text]).to eq("text 2")
+  end
+
   it "accept escaped quotes" do
     resource_file = '"\"label\"1\"" = "\"text\"1\"";'
     entries = IphoneParser.parse(resource_file)
